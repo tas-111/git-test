25f00fac1bdf0d93927cb31c8b57aadc66efdab1
Aaron <Aaron@tas.com>
2021-05-27 14:04:35 +0800

    hot-fix commit2

--- src/main/java/com/tas/git/GitTest.java
+++ src/main/java/com/tas/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello GitHub!");
         System.out.println("hello Aaron");
         System.out.println("hello Tan'sir");
+        System.out.println("hot fix");
     }
 }
